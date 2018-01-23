95f0e3f1e7876ed03220479a5c5640e6af31f2e0
Elzbeth Blanco <elzbethblanco@gmail.com>
2018-01-23 07:46:34 -0600

    first commit
    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# FoodMaps
+* **Track:** _Common Core_
+* **Sprint 03:** _JQuery_
+* **Unidad:** _04_
+
+## Objetivo
+Crear una web-app que a través de un input pueda filtar los restaurantes que se encuentran cerca del usuario.
+
+## Flujo de la aplicación:
+Vista splash con duración de 2 a 5 segundos que redirecciona a la vista de principal.
+
+![start](docs/images/splash.jpg)
+
+En la vista principal se muestran todos los restaurantes cercanos junto con el input para filtrar la selección del usuario.
+Los criterios de busqueda pueden ser por tipo de comida, costo, orden alfabético, etc.
+
+![near](docs/images/2.jpg )
+
+Una vez que el usuario haga su elección deberán mostrarse únicamente aquellos restaurantes que cumplan con la condición.
+
+![filtered](docs/images/3.jpg )
+
+Se debe agregar un evento mouse over sobre las imágenes, puede mostrar por ejemplo: cambio de tamaño, mostrar un texto, cambio de imagen, información, etc.
+
+![mouseover](docs/images/4.jpg)
+
+Al seleccionar alguno de los restaurantes, deberá mostrarse la información de este a través de un modal.
+
+![info](docs/images/5.jpg )
+
+Una vez cerrado el modal, regresa a la vista principal.
+
+![back](docs/images/6.jpg)
+
+
+## Flujo de trabajo
+
+1. Establecer estructura de carpetas con los archivos necesarios para el proyecto.
+
+2. Escribir pseudocódigo para identificar funcionalidad requerida.
+
+3. Realizar el marcado del contenido en un archivo html.
+
+4. Desarrollar funcionalidad.
+
+5. Establecer estilos en archivo css.
+
+6. Aplicar los principios de diseño responsive para completar el objetivo del ejercicio.
+
+5. Crear archivo README.
+
+7. Subir a repositorio Git y crear GHpage correspondiente.
---
+++ js/app.js
@@ -0,0 +1,122 @@
+//Variables//
+
+// Loading Document//
+$(document).ready(function(e){
+  //Modals and Select//
+  $('select').material_select(); //activates selections in modals
+  $('.modal').modal();
+  //Modals and Select End//
+  //Splash/Main Page//
+  $("#main-page").hide();
+  $('#splash').fadeOut(2000, function() {
+  $("#splash").remove();
+  $("#main-page").fadeIn(1000);
+  });
+  //Splash/Main Page End//
+  //Search Bar & Filtering//
+  $('#search').keyup(function() {
+    var $searching = $(this).val().toLowerCase();
+    $('.restaurants').hide();
+    $('.restaurants').each(function() {
+      var filter = $(this).data('name');
+      if (filter.indexOf($searching) !== -1 && $searching.length !== 0) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+  //Filtering End//
+  //Mouseover Event//
+
+
+
+
+
+  //Mouseover Event End//
+  //Restaurant Info//
+
+
+
+
+
+  //Restaurant Info End//
+  //Map//
+  var options = {
+      zoom: 8,
+      center: new google.maps.LatLng(19.2, -61.4),
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      backgroundColor: '#ffffff',
+      noClear: true,
+      disableDefaultUI: true,
+      keyboardShortcuts: false,
+      disableDoubleClickZoom: true,
+      draggable: false,
+      scrollwheel: false,
+      draggableCursor: 'move',
+      draggingCursor: 'move',
+      mapTypeControl: true,
+
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_MENU,
+        position: google.maps.ControlPosition.TOP_LEFT,
+        mapTypeIds: [
+          google.maps.MapTypeId.SATELLITE
+        ]
+      },
+      navigationControl: true,
+      streetViewControl: true,
+      navigationControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT,
+        style: google.maps.NavigationControlStyle.ANDROID
+      },
+      scaleControl: true,
+      scaleControlOptions: {
+        position: google.maps.ControlPosition.TOP_LEFT,
+        style: google.maps.ScaleControlStyle.DEFAULT
+      }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    map.setOptions({
+      zoom: 10,
+      center: new google.maps.LatLng(18.17, -66.42),
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      keyboardShortcuts: true,
+      disableDoubleClickZoom: false,
+      draggable: true,
+      scrollwheel: true,
+      draggableCursor: 'hand',
+      draggingCursor: 'hand',
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        position: google.maps.ControlPosition.TOP_RIGHT,
+        mapTypeIds: [
+          google.maps.MapTypeId.ROADMAP,
+          google.maps.MapTypeId.SATELLITE
+        ]
+      },
+      navigationControlOptions: {
+        position: google.maps.ControlPosition.TOP_LEFT,
+        style: google.maps.NavigationControlStyle.ZOOM_PAN
+      },
+      scaleControlOptions: {
+        position: google.maps.ControlPosition.BOTTOM_LEFT,
+        style: google.maps.ScaleControlStyle.DEFAULT
+      }
+    });
+
+    map.setZoom(9);
+    var zoomLevel = map.getZoom();
+
+    map.setCenter(new google.maps.LatLng(18.17, -66.3));
+    var centerOfMap = map.getCenter();
+
+    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+    var mapTypeIdOfMap = map.getMapTypeId();
+
+  //Map End//
+
+
+});
